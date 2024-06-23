755819d79e97e5ad5a93fbcb7e184647b1ab3454
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 02:36:00 +0530

    feat: sidebar

---
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { MenuLinks } from "@/data/links";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <aside className="w-[25%] hidden lg:flex">
+      <ul className="w-full flex flex-col gap-2">
+        {MenuLinks.map((link) => (
+          <Link key={link.id} to={link.url}>
+            <li
+              className={` ${
+                location.pathname === link.url
+                  ? "bg-blue-200 text-blue-600 border-l-4 border-blue-700"
+                  : "text-gray-500"
+              } group cursor-pointer p-2 text-sm font-medium w-full hover:bg-gray-200 flex gap-2  items-center`}
+            >
+              <span className="group-hover:text-gray-800">{link.icon}</span>
+              <p className=" whitespace-nowrap group-hover:text-gray-800">
+                {link.name}
+              </p>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
