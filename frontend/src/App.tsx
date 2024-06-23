ac436f998464c37f822ac30ef7c800883873c2a0
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-23 17:57:10 +0530

    init: CollegeBuzz TS, Tailwind, Shadcn setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="min-h-dvh flex flex-col items-center justify-center ">
+        <h1 className=" text-4xl font-bold">College<span className=" text-blue-600">Buzz</span></h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
