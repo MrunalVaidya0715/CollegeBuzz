99b989feeeea930b38c5ae9fb8276de5665be72c
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 03:07:05 +0530

    style: sticky Sidebar

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <aside className="w-[25%] hidden lg:flex">
+    <aside className="sticky top-[4.5rem] w-[25%] h-fit hidden lg:flex">
       <ul className="w-full flex flex-col gap-2">
         {MenuLinks.map((link) => (
           <Link key={link.id} to={link.url}>
