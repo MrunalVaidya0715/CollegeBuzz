456f7341ae642fb1911a1a902473657d75b264af
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 02:47:37 +0530

    feat: nested Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import NotFound from "./pages/not-found/NotFound";
+import Posts from "./pages/posts/Posts";
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
         <Navbar />
         <main className="w-full min-h-dvh flex flex-col items-center justify-between ">
           <Routes>
-            <Route path="/" element={<Home />} />
-
+            <Route path="/" element={<Home />}>
+              <Route path="" element={<Posts />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
