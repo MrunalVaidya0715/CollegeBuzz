e3585c703477e966272be9a62fede6702a29442f
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 02:47:56 +0530

    feat: added Outlet

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,16 +1,17 @@
-import Sidebar from "@/components/sidebar/Sidebar"
+import Sidebar from "@/components/sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="pt-16 w-full min-h-dvh flex justify-center" >
-        <div className="p-2 w-full max-w-[1200px] flex justify-between">
-        <Sidebar/>
-        <div className=" w-full"></div>
-        
+    <div className="pt-16 w-full min-h-dvh flex justify-center">
+      <div className="p-2 w-full max-w-[1200px] flex justify-between">
+        <Sidebar />
+        <div className=" overflow-y-auto h-full w-full flex flex-col  p-2 scroll-smooth">
+          <Outlet />
         </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
