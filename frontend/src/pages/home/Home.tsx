12e0b40b0525d7e55653744b49781b82779c8df7
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 02:37:47 +0530

    feat: display Sidebar

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,6 +1,15 @@
+import Sidebar from "@/components/sidebar/Sidebar"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="pt-16 w-full min-h-dvh flex justify-center" >
+        <div className="p-2 w-full max-w-[1200px] flex justify-between">
+        <Sidebar/>
+        <div className=" w-full"></div>
+        
+        </div>
+        
+    </div>
   )
 }
 
