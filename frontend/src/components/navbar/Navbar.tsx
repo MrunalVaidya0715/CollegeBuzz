a76be924d428c4c6694c477a1608427a27d058f7
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 01:57:56 +0530

    feat: Navbar

---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,131 @@
+import { useState } from "react";
+import { BiSearch } from "react-icons/bi";
+import { AiOutlineHome } from "react-icons/ai";
+import { MdOutlineExplore, MdOutlineLogout } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { LiaHandsHelpingSolid } from "react-icons/lia";
+import { Button } from "../ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Link } from "react-router-dom";
+
+const MenuLinks = [
+  {
+    id: 1,
+    name: "Home",
+    url: "/",
+    icon: <AiOutlineHome size={22} />,
+  },
+  {
+    id: 2,
+    name: "Explore Questions",
+    url: "/explore",
+    icon: <MdOutlineExplore size={22} />,
+  },
+  {
+    id: 3,
+    name: "Contribute",
+    url: "/contribute",
+    icon: <LiaHandsHelpingSolid size={22} />,
+  },
+];
+
+const Navbar = () => {
+  const user = true;
+  const [searchText, setSearchText] = useState("");
+  const handleSearchTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+  const [isPopOpen, setIsPopOpen] = useState(false);
+  return (
+    <header className="z-[100] fixed top-0 left-0 h-16 w-full flex justify-center bg-white border-b-1 border-gray-300 shadow-md">
+      <nav className="relative p-2 w-full h-full max-w-[1200px] flex items-center justify-between">
+        {/* Logo */}
+        <Link to={"/"} aria-label="CollegeBuzz Home">
+          <h1 className="flex text-lg font-bold">
+            C<span className="hidden sm:block">ollege</span>
+            <span className=" text-blue-600 flex">
+              B<span className="hidden sm:block">uzz</span>
+            </span>
+          </h1>
+        </Link>
+        {/* Search */}
+        <div className="absolute left-1/2 -translate-x-1/2 px-2 py-1 h-9 flex items-center gap-2 text-gray-400 bg-gray-100 border-1 border-gray-200  rounded-lg">
+          <BiSearch className=" text-[20px] md:text-[22px]" />
+          <input
+            value={searchText}
+            onChange={handleSearchTextChange}
+            className="p-1 text-black text-sm md:text-base w-full bg-transparent outline-none rounded-md"
+            type="text"
+            placeholder="Search by title"
+          />
+        </div>
+        {/* Login/User */}
+        <section>
+          {user ? (
+            <div
+              className={`w-[35px] aspect-square border-1 ${
+                isPopOpen ? "border-black" : "border-gray-300"
+              } rounded-full overflow-hidden transition-colors duration-200 ease-in-out`}
+            >
+              <Popover open={isPopOpen} onOpenChange={setIsPopOpen}>
+                <PopoverTrigger>
+                  <img
+                    className=" w-full h-full object-cover object-center"
+                    src="/assets/no-profile.png"
+                    alt="no-profile-img"
+                  />
+                </PopoverTrigger>
+                <PopoverContent className="mr-2 translate-y-4 max-w-[250px] ">
+                  <ul className=" w-full flex flex-col gap-2">
+                    {MenuLinks.map((mLink) => (
+                      <Link
+                        onClick={() => setIsPopOpen(false)}
+                        key={mLink.name}
+                        to={mLink.url}
+                      >
+                        <li className=" group p-2 text-sm md:text-base font-medium w-full hover:bg-gray-100 flex gap-2 text-gray-400 items-center transition-all duration-200 ease-in-out">
+                          <span className="group-hover:text-gray-800">
+                            {mLink.icon}
+                          </span>
+                          <p className="group-hover:text-gray-800">
+                            {mLink.name}
+                          </p>
+                        </li>
+                      </Link>
+                    ))}
+
+                    <Link onClick={() => setIsPopOpen(false)} to={"/profile"}>
+                      <li className=" group p-2 text-sm md:text-base font-medium w-full hover:bg-gray-100 flex gap-2 text-gray-400 items-center transition-all duration-200 ease-in-out">
+                        <span className="group-hover:text-gray-800">
+                          <CgProfile size={22} />
+                        </span>
+                        <p className="group-hover:text-gray-800">Profile</p>
+                      </li>
+                    </Link>
+                    <li
+                      onClick={() => setIsPopOpen(false)}
+                      className=" group cursor-pointer p-2 text-sm md:text-base font-medium w-full hover:bg-gray-100 flex gap-2 text-gray-400 items-center transition-all duration-200 ease-in-out"
+                    >
+                      <span className="group-hover:text-gray-800">
+                        <MdOutlineLogout size={22} />
+                      </span>
+                      <p className="group-hover:text-gray-800">Logout</p>
+                    </li>
+                  </ul>
+                </PopoverContent>
+              </Popover>
+            </div>
+          ) : (
+            <Button variant={"outline"}  aria-label="Login">Login</Button>
+          )}
+        </section>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
