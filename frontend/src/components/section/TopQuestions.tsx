35444c88c5ba9ed0f452093b3e69e69b4c4c2941
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 18:50:01 +0530

    feat: TopQuestion Section

---
+++ frontend/src/components/section/TopQuestions.tsx
@@ -0,0 +1,18 @@
+import TopQuestionCard from "@/cards/TopQuestionCard";
+
+const TopQuestions = () => {
+  return (
+    <div className="pt-4 p-3 w-full h-auto flex flex-col gap-4 bg-white border-1 border-gray-300">
+      <h2 className=" text-center font-medium">Top Questions</h2>
+      <div className="w-full flex flex-col gap-4">
+        {Array(5)
+          .fill(null)
+          .map((_, index) => (
+            <TopQuestionCard key={index} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopQuestions;
