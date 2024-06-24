fdc0ac68cba30243046824fa0b695008fe4ffc8d
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-25 00:45:44 +0530

    feat: PostCard Component

--- frontend/src/pages/posts/Posts.tsx
+++ frontend/src/pages/posts/Posts.tsx
@@ -1,10 +1,12 @@
+import PostCard from "@/cards/PostCard";
+
 const Posts = () => {
   return (
     <div className="w-full flex flex-col gap-4">
       {Array(20)
         .fill(null)
         .map((_, index) => (
-          <div key={index} className="w-full h-[150px] border-1 border-gray-400 bg-slate-100"></div>
+          <PostCard key={index} />
         ))}
     </div>
   );
