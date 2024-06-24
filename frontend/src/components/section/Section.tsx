a95f34840e6543e0e6f7c9073df3e98f2516a211
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 18:51:36 +0530

    feat: Section component

---
+++ frontend/src/components/section/Section.tsx
@@ -0,0 +1,21 @@
+import { Button } from "../ui/button";
+import { IoMdAdd } from "react-icons/io";
+import TopQuestions from "./TopQuestions";
+const Section = () => {
+  return (
+    <aside className="sticky top-[5rem] w-[30%] h-fit hidden md:flex flex-col gap-6">
+      <Button
+        className=" bg-blue-600 hover:bg-blue-800 gap-2"
+        aria-label="Ask Question"
+      >
+        <IoMdAdd className=" w-5 h-5" />
+        Ask Question
+      </Button>
+      <section>
+        <TopQuestions/>
+      </section>
+    </aside>
+  );
+};
+
+export default Section;
