3e24534e38bd0311bd6ad66a6a0d44f7884124c9
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 18:52:16 +0530

    feat: added SelectOptions data

--- frontend/src/data/links.tsx
+++ frontend/src/data/links.tsx
@@ -4,22 +4,70 @@ import { AiOutlineHome } from "react-icons/ai";
 import { MdOutlineExplore } from "react-icons/md";
 
 export const MenuLinks = [
-    {
-      id: 1,
-      name: "Home",
-      url: "/",
-      icon: <AiOutlineHome size={22} />,
-    },
-    {
-      id: 2,
-      name: "Explore Questions",
-      url: "/explore",
-      icon: <MdOutlineExplore size={22} />,
-    },
-    {
-      id: 3,
-      name: "Contribute",
-      url: "/contribute",
-      icon: <LiaHandsHelpingSolid size={22} />,
-    },
-  ];
+  {
+    id: 1,
+    name: "Home",
+    url: "/",
+    icon: <AiOutlineHome size={22} />,
+  },
+  {
+    id: 2,
+    name: "Explore Questions",
+    url: "/explore",
+    icon: <MdOutlineExplore size={22} />,
+  },
+  {
+    id: 3,
+    name: "Contribute",
+    url: "/contribute",
+    icon: <LiaHandsHelpingSolid size={22} />,
+  },
+];
+
+export const FilterOptions = [
+  {
+    id: 1,
+    name: "All",
+    value: "all"
+  },
+  {
+    id: 2,
+    name: "General",
+    value: "general"
+  },
+  {
+    id: 3,
+    name: "Technology",
+    value: "technology"
+  },
+  
+  {
+    id: 4,
+    name: "Career",
+    value: "career"
+  },
+  
+  {
+    id: 5,
+    name: "Sports",
+    value: "sports"
+  },
+  
+  {
+    id: 6,
+    name: "Faculty",
+    value: "faculty"
+  },
+  
+  {
+    id: 7,
+    name: "Canteen",
+    value: "canteen"
+  },
+  {
+    id: 8,
+    name: "Academics",
+    value: "academics"
+  },
+
+];
