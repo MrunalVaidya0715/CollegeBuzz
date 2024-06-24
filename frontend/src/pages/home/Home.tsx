9e7771d6712920180569f13c509f40b0103ed42a
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 18:55:49 +0530

    refactor: px-2

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div className="pt-16 w-full min-h-dvh flex justify-center">
       <div className="pt-0 lg:pt-4 md:p-2 w-full max-w-[1200px] flex justify-between gap-4">
         <Sidebar />
-        <div className=" h-auto w-full flex flex-col justify-start gap-1 lg:gap-2  px-2 scroll-smooth">
+        <div className="  px-2 h-auto w-full flex flex-col justify-start gap-1 lg:gap-2  scroll-smooth">
           <FilterSort />
           <div className="flex-1 overflow-y-auto">
             <Outlet />
