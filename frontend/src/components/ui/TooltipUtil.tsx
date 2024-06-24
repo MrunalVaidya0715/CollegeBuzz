a7a7f06d3ad74c923e8e676206de970590df3d64
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-24 19:22:30 +0530

    feat: custom TootlTip Util

---
+++ frontend/src/components/ui/TooltipUtil.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface TooltipUtilProps {
+  content: string;
+  children: React.ReactNode;
+}
+
+const TooltipUtil = ({ content, children }:TooltipUtilProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent>
+          <p>{content}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default TooltipUtil;
