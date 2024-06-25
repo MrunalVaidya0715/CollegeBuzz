3cf77607676480e0680e94daafaaab653d3329e6
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-25 09:06:56 +0530

    feat: User model Schema

---
+++ api/src/models/user.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  profileImg: string;
+  role: "admin" | "user";
+  createdAt?: Date | string;
+  updatedAt?: Date | string;
+}
+
+const userSchema: Schema<IUser> = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profileImg: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IUser>("User", userSchema);
