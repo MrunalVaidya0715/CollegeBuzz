2d1f36b3daf7e1f6239e8c1892b4cd1a9a35e73c
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-25 06:23:49 +0530

    style: responsive Dialog

--- frontend/src/components/dialogs/AnswerDialog.tsx
+++ frontend/src/components/dialogs/AnswerDialog.tsx
@@ -17,7 +17,7 @@ interface AnswerDialogProps {
 const AnswerDialog = ({ isAddAnsOpen, setIsAddAnsOpen }: AnswerDialogProps) => {
   return (
     <AlertDialog open={isAddAnsOpen} onOpenChange={setIsAddAnsOpen}>
-      <AlertDialogContent className="py-4 w-[95%] max-w-[550px] min-h-[300px] flex flex-col rounded-lg">
+      <AlertDialogContent className="py-4 w-[95%] max-w-[550px] min-h-[300px] max-h-[calc(100dvh-100px)] flex flex-col rounded-lg overflow-y-auto">
         <AlertDialogCancel className=" absolute right-2 top-0 md:top-2 w-fit border-transparent hover:border-gray-300 ">
           <AiOutlineClose className=" w-5 h-5" />
         </AlertDialogCancel>
