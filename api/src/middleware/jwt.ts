5939ec268c2e5a071dca53b634a4d36a7d0375fc
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-25 09:06:10 +0530

    feat: jwt verifyToken middleware

---
+++ api/src/middleware/jwt.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import createError from "../utils/createError.js";
+import jwt, { JwtPayload, VerifyErrors } from "jsonwebtoken";
+
+interface CustomRequest extends Request {
+  userId?: string;
+  role?: string;
+}
+
+interface CustomJwtPayload extends JwtPayload {
+  id: string;
+  role: string;
+}
+
+export const verifyToken = (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.accessToken;
+  if (!token) return next(createError(401, "You are not Authenticated"));
+
+  jwt.verify(
+    token,
+    process.env.JWT_KEY as string,
+    (err: VerifyErrors | null, payload: any) => {
+      if (err || !payload) {
+        return next(createError(403, "Token is Invalid"));
+      }
+
+      req.userId = payload.id;
+      req.role = payload.role;
+      next();
+    }
+  );
+};
