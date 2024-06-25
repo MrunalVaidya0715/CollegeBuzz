085d14f67a5bae6d819d899a3af5e513c6bd9105
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-25 10:16:28 +0530

    feat: handleGoogleLogin

---
+++ api/src/controllers/auth.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken"
+import User, { IUser } from "../models/user.model";
+
+export const handleGoogleLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, username, profileImg, exp} = req.body;
+
+    let user: IUser | null = await User.findOne({ email });
+
+    if (!user) {
+      const newUser = new User({
+        username,
+        email,
+        profileImg,
+      });
+
+      user = await newUser.save();
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_KEY as string,
+      { expiresIn: exp }
+    );
+
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      })
+      .status(201)
+      .send(user);
+  } catch (error) {
+    next(error);
+  }
+};
