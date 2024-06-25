670e71289280607eda115aca61ac1a3d522e5f05
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-25 11:25:51 +0530

    feat: show my-question link when user

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,9 @@
 import { MenuLinks } from "@/data/links";
+import useAuthStore from "@/store/useAuth";
 import { Link, useLocation } from "react-router-dom";
-
+import { RiQuestionAnswerLine } from "react-icons/ri";
 const Sidebar = () => {
+  const user = useAuthStore((state) => state.user);
   const location = useLocation();
   return (
     <aside className="sticky top-[5rem] w-[30%] h-fit hidden lg:flex">
@@ -22,6 +24,22 @@ const Sidebar = () => {
             </li>
           </Link>
         ))}
+        {user && (
+          <Link to={"my-questions"}>
+            <li
+              className={` ${
+                location.pathname === "/my-questions"
+                  ? "bg-blue-200 text-blue-600 border-l-4 border-blue-700"
+                  : "text-gray-500"
+              } group cursor-pointer p-2 text-sm font-medium w-full hover:bg-gray-200 flex gap-2  items-center`}
+            >
+              <span className="group-hover:text-gray-800"><RiQuestionAnswerLine className=" w-[20px] h-[20px]"/></span>
+              <p className=" whitespace-nowrap group-hover:text-gray-800">
+                My Questions
+              </p>
+            </li>
+          </Link>
+        )}
       </ul>
     </aside>
   );
