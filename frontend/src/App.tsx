5e0847d90d76e696d2b39acb91bfbd7dd7816c75
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-26 07:27:37 +0530

    feat: Added QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import NotFound from "./pages/not-found/NotFound";
@@ -11,24 +12,27 @@ import { Toaster } from "./components/ui/toaster";
 
 function App() {
   const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  const queryClient = new QueryClient();
   return (
     <>
       <Router>
-        <GoogleOAuthProvider clientId={clientId}>
-          <Navbar />
-          <main className="w-full min-h-dvh flex flex-col items-center justify-between ">
-            <Routes>
-              <Route path="/" element={<Home />}>
-                <Route path="" element={<Posts />} />
-                <Route path="posts/:id" element={<PostDetail />} />
-                <Route path="explore" element={<Explore />} />
-              </Route>
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-            <ScrollToTopOnPageChange />
-            <Toaster/>
-          </main>
-        </GoogleOAuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <GoogleOAuthProvider clientId={clientId}>
+            <Navbar />
+            <main className="w-full min-h-dvh flex flex-col items-center justify-between ">
+              <Routes>
+                <Route path="/" element={<Home />}>
+                  <Route path="" element={<Posts />} />
+                  <Route path="posts/:id" element={<PostDetail />} />
+                  <Route path="explore" element={<Explore />} />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+              <ScrollToTopOnPageChange />
+              <Toaster />
+            </main>
+          </GoogleOAuthProvider>
+        </QueryClientProvider>
       </Router>
     </>
   );
