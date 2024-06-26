75aeff6e9e1550555edf62533f0e3d4bad5df484
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-26 13:10:37 +0530

    feat: added isAskOpen control

--- frontend/src/store/useDialogStore.ts
+++ frontend/src/store/useDialogStore.ts
@@ -3,11 +3,19 @@ import { create } from "zustand";
 interface DialogState {
   isLoginOpen: boolean;
   setIsLoginOpen: (isOpen: boolean) => void;
+  isDrawerOpen: boolean;
+  setIsDrawerOpen: (isOpen: boolean) => void;
+  isAskQuesOpen: boolean;
+  setIsAskQuesOpen: (isOpen: boolean) => void;
 }
 
 const useDialogStore = create<DialogState>()((set) => ({
   isLoginOpen: false,
   setIsLoginOpen: (isOpen) => set({ isLoginOpen: isOpen }),
+  isDrawerOpen: false,
+  setIsDrawerOpen: (isOpen) => set({ isDrawerOpen: isOpen }),
+  isAskQuesOpen: false,
+  setIsAskQuesOpen: (isOpen) => set({ isAskQuesOpen: isOpen }),
 }));
 
 export default useDialogStore;
