1ea57c01b895c19157cbdfdd72817c9bff5625a3
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-29 00:38:15 +0530

    fear: temp rm handleVotes controllers

--- api/src/routes/question.route.ts
+++ api/src/routes/question.route.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
-import { createQuestion, embedQuestion, getQuestionById, getQuestions, getSimilarQuestions, handleDownVote, handleUpVote } from '../controllers/question.controller';
+import { createQuestion, embedQuestion, getQuestionById, getQuestions, getSimilarQuestions } from '../controllers/question.controller';
 import { verifyToken } from '../middleware/jwt';
 const router  = Router()
 
 router.post('/embed-question', embedQuestion);
 router.post('/create-question', verifyToken, createQuestion)
 router.post('/find-similar-questions', getSimilarQuestions)
-router.put('/upvote/:id', verifyToken, handleUpVote);
-router.put('/downvote/:id', verifyToken, handleDownVote);
 router.get('/single/:id', getQuestionById)
 router.get('/', getQuestions)
 
