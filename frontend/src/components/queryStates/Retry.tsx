203166d82a2ddcc0e5e1a08598c52b5c06569c44
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-29 05:32:48 +0530

    feat: Retry className prop

--- frontend/src/components/queryStates/Retry.tsx
+++ frontend/src/components/queryStates/Retry.tsx
@@ -2,15 +2,25 @@ import { BsArrowCounterclockwise } from "react-icons/bs";
 import { Button } from "../ui/button";
 import { QueryObserverResult, RefetchOptions } from "@tanstack/react-query";
 
-interface RetryProps{
-    refetch: (options?: RefetchOptions | undefined) => Promise<QueryObserverResult<unknown[] | unknown, Error>>
+interface RetryProps {
+  refetch: (
+    options?: RefetchOptions | undefined
+  ) => Promise<QueryObserverResult<unknown[] | unknown, Error>>;
+  className?: string;
 }
 
-const Retry = ({refetch}:RetryProps) => {
+const Retry = ({ refetch, className }: RetryProps) => {
   return (
-    <section className=" w-full min-h-[400px] flex flex-col items-center justify-center gap-2">
+    <section
+      className={`${
+        className ? className : "min-h-[400px]"
+      } w-full  flex flex-col items-center justify-center gap-2`}
+    >
       <p className=" font-medium text-gray-500">Something went wrong</p>
-      <Button onClick={()=>refetch()} className=" gap-2 hover:text-yellow-400">
+      <Button
+        onClick={() => refetch()}
+        className=" gap-2 hover:text-yellow-400"
+      >
         Retry
         <BsArrowCounterclockwise className=" w-5 h-5" />
       </Button>
