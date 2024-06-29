6d0f12deee2bd4aa8d095117b6aaa54f9b329c1d
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-06-29 05:33:03 +0530

    feat: display topQuestions

--- frontend/src/cards/TopQuestionCard.tsx
+++ frontend/src/cards/TopQuestionCard.tsx
@@ -1,18 +1,33 @@
-import { TiArrowSortedUp } from "react-icons/ti";
-const TopQuestionCard = () => {
+import { TopQuestion } from "@/components/section/TopQuestions";
+import { TiArrowSortedUp, TiArrowSortedDown } from "react-icons/ti";
+import { GoDash } from "react-icons/go";
+import { Link } from "react-router-dom";
+interface TQuesCardProps {
+  index: number;
+  tQues: TopQuestion;
+}
+
+const TopQuestionCard = ({ index, tQues }: TQuesCardProps) => {
+  const vote = tQues.vote;
   return (
-    <div className=" flex items-center gap-1">
-      <p className=" font-medium text-sm mr-1">1.</p>
-      <div className=" col-span-4 line-clamp-1">
-        <p className="cursor-pointer text-sm underline-offset-2 hover:underline hover:text-blue-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          facilis assumenda earum repellat iusto impedit inventore sequi
-          officiis et repudiandae.
-        </p>
-      </div>
-      <div className="relative flex flex-col items-center">
-        <TiArrowSortedUp className="absolute -top-[50%] text-blue-500" />
-        <p className=" font-medium text-sm">4</p>
+    <div className="w-full flex items-center justify-between gap-1">
+      <p className=" font-medium text-sm mr-1">{index + 1}.</p>
+      <Link to={`/posts/${tQues._id}`} className=" w-full">
+        <div className="w-full line-clamp-1">
+          <p className="cursor-pointer text-sm underline-offset-2 hover:underline hover:text-blue-500">
+            {tQues.title}
+          </p>
+        </div>
+      </Link>
+      <div className=" flex items-center gap-1">
+        <p className=" font-medium text-sm">{vote > 0 ? vote : vote * 1}</p>
+        {vote > 0 ? (
+          <TiArrowSortedUp className=" text-blue-600" />
+        ) : vote < 0 ? (
+          <TiArrowSortedDown className=" text-red-600" />
+        ) : (
+          <GoDash className="w-3 text-gray-600" />
+        )}
       </div>
     </div>
   );
--- frontend/src/components/section/TopQuestions.tsx
+++ frontend/src/components/section/TopQuestions.tsx
@@ -1,15 +1,42 @@
 import TopQuestionCard from "@/cards/TopQuestionCard";
+import apiRequest from "@/lib/apiRequest";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "../ui/skeleton";
+import Retry from "../queryStates/Retry";
+
+export interface TopQuestion {
+  _id: string;
+  title: string;
+  vote: number;
+}
 
 const TopQuestions = () => {
+  const {
+    data: topQues,
+    isLoading,
+    isRefetching,
+    error,
+    refetch,
+  } = useQuery<TopQuestion[]>({
+    queryKey: ["topQuestions"],
+    queryFn: () =>
+      apiRequest.get(`questions/top-questions`).then((res) => {
+        return res.data;
+      }),
+  });
   return (
-    <section className="pt-4 p-3 w-full h-auto flex flex-col gap-4 bg-white border-1 border-gray-200">
+    <section className="py-4 pl-3 pr-2 w-full h-auto flex flex-col gap-4 bg-white border-1 border-gray-200">
       <h2 className=" text-center font-medium">Top Questions</h2>
       <div className="w-full flex flex-col gap-5">
-        {Array(5)
-          .fill(null)
-          .map((_, index) => (
-            <TopQuestionCard key={index} />
-          ))}
+        {isLoading || isRefetching ? (
+          Array(4)
+            .fill(null)
+            .map((_, index) => <Skeleton key={index} className=" w-full h-6" />)
+        ) : error ? (
+          <Retry refetch={refetch} className="min-h-[200px]" />
+        ) : (
+          topQues?.map((ques, index) => <TopQuestionCard key={ques._id} index={index} tQues={ques} />)
+        )}
       </div>
     </section>
   );
