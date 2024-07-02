a3cc8b63dbad2235fa631713fca0bb8a72e89a5a
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-07-02 15:22:23 +0530

    style gap-5 -> gap-3 bw tQuestions

--- frontend/src/components/section/TopQuestions.tsx
+++ frontend/src/components/section/TopQuestions.tsx
@@ -26,7 +26,7 @@ const TopQuestions = () => {
   return (
     <section className="py-4 pl-3 pr-2 w-full h-auto flex flex-col gap-4 bg-white border-1 border-gray-200">
       <h2 className=" text-center font-medium">Top Questions</h2>
-      <div className="w-full flex flex-col gap-5">
+      <div className="w-full flex flex-col gap-4">
         {isLoading ? (
           Array(4)
             .fill(null)
