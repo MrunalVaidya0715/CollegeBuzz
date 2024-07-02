667bf11c34d39e18a5693bb7e05045159d123a56
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-07-02 15:21:16 +0530

    feat: axios interceptor to detect Session Expiry

--- frontend/src/lib/apiRequest.ts
+++ frontend/src/lib/apiRequest.ts
@@ -1,8 +1,25 @@
+import { toast} from "@/components/ui/use-toast";
+import useAuthStore from "@/store/useAuth";
 import axios from "axios";
 
 const apiRequest = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true
 });
+
+apiRequest.interceptors.response.use(
+    response => response,
+    error => {
+        
+        const { status } = error.response;
+        if (status === 401) {
+            useAuthStore.getState().clearUser();
+            toast({variant:"destructive",title:"Session Expired", description:"Please login again"});
+            
+        }
+        return Promise.reject(error);
+    }
+);
+
 
 export default apiRequest;
