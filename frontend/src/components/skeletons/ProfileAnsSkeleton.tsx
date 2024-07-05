8ed26cacd414ed813fa4abe03604044b36d07262
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-07-05 18:04:38 +0530

    feat: added votes Skeleton

--- frontend/src/components/skeletons/ProfileAnsSkeleton.tsx
+++ frontend/src/components/skeletons/ProfileAnsSkeleton.tsx
@@ -5,6 +5,7 @@ const ProfileAnsSkeleton = () => {
     <div className=" p-4 w-full min-h-[100px] flex flex-col gap-2 bg-white border-1 border-gray-200">
       <Skeleton className=" h-8 w-full" />
       <Skeleton className=" h-8 w-full" />
+      <Skeleton className=" h-8 w-1/4" />
     </div>
   );
 };
