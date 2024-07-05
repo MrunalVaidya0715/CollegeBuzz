a9110bc91e203e931afb55ba2d43359b17ab9e29
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-07-05 17:05:36 +0530

    feat: getUser controller

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -3,8 +3,21 @@ import { CustomRequest } from "../middleware/jwt";
 import Answer from "../models/answer.model";
 import Question from "../models/question.model";
 import createError from "../utils/createError";
+import User from "../models/user.model";
 
-
+export const getUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId).select("username profileImg createdAt");
+    res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getUserProfileQuestions = async (
   req: Request,
--- api/src/routes/user.route.ts
+++ api/src/routes/user.route.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { getUserProfileQuestions } from '../controllers/user.controller';
+import { getUser, getUserProfileQuestions } from '../controllers/user.controller';
 const router  = Router()
 
+router.get('/:userId', getUser);
 router.get('/profile-questions/:userId', getUserProfileQuestions)
 
 export default router;
