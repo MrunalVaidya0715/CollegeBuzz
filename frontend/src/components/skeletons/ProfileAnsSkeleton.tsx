8404cafc508c6267356aa193c7b39447a0b82c6b
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-07-05 17:53:44 +0530

    feat: Profile Answer Skeleton

---
+++ frontend/src/components/skeletons/ProfileAnsSkeleton.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from "../ui/skeleton";
+
+const ProfileAnsSkeleton = () => {
+  return (
+    <div className=" p-4 w-full min-h-[100px] flex flex-col gap-2 bg-white border-1 border-gray-200">
+      <Skeleton className=" h-8 w-full" />
+      <Skeleton className=" h-8 w-full" />
+    </div>
+  );
+};
+
+export default ProfileAnsSkeleton;
