184bdd857c5685789ffb06124818a04409af7ce9
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-07-10 20:00:00 +0530

    feat: added 10min refetch topQuestion interval

--- frontend/src/components/section/TopQuestions.tsx
+++ frontend/src/components/section/TopQuestions.tsx
@@ -22,7 +22,8 @@ const TopQuestions = () => {
       apiRequest.get(`questions/top-questions`).then((res) => {
         return res.data;
       }),
-      staleTime: 10 * 60 * 1000
+      staleTime: 10 * 60 * 1000,
+      refetchInterval: 10 * 60 * 1000
   });
   return (
     <section className="py-4 pl-3 pr-2 w-full h-auto flex flex-col gap-4 bg-white border-1 border-gray-200">
