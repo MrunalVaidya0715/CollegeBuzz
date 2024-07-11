35f70a7010cf1fafa907e5713db92d4624598b02
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-07-12 00:49:59 +0530

    feat: added Infinite Scroll

--- frontend/src/pages/posts/Posts.tsx
+++ frontend/src/pages/posts/Posts.tsx
@@ -1,11 +1,13 @@
+import { useEffect } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
 import PostCard from "@/cards/PostCard";
 import NoData from "@/components/queryStates/NoData";
 import Retry from "@/components/queryStates/Retry";
 import apiRequest from "@/lib/apiRequest";
 import PostCardSkeleton from "@/components/skeletons/PostCardSkeleton";
 import useQuestionFilterStore from "@/store/useFilterSortStore";
-import { useQuery } from "@tanstack/react-query";
-import { useEffect} from "react";
+import { LucideLoaderCircle } from "lucide-react";
 
 interface UserId {
   _id: string;
@@ -14,6 +16,7 @@ interface UserId {
   profileImg: string;
   role: string;
 }
+
 export interface Post {
   _id: string;
   userId: UserId;
@@ -26,48 +29,82 @@ export interface Post {
   downvote: number;
   isDownvoted: Map<string, boolean>;
   report: number;
-  reportedBy: { userId: string, reason: string }[];
+  reportedBy: { userId: string; reason: string }[];
   answers: string[];
   embedding?: number[];
   createdAt: string;
   updatedAt: string;
 }
 
+interface PostFetched {
+  questions: Post[];
+  totalPages: number;
+  currentPage: number;
+}
+
 const Posts = () => {
   const { category, sortBy } = useQuestionFilterStore();
   const catQuery = category;
-  const sortQuery =  sortBy;
-
+  const sortQuery = sortBy;
+  const { ref, inView } = useInView({ threshold: 1 });
+  const fetchPosts = async ({ pageParam = 1 }) => {
+    const res = await apiRequest.get(
+      `questions?category=${catQuery}&sortBy=${sortQuery}&page=${pageParam}`
+    );
+    return res.data as PostFetched;
+  };
   const {
-    data: posts,
+    data,
     isLoading,
     isRefetching,
-    error,
+    isError,
     refetch,
-  } = useQuery<Post[]>({
+    isSuccess,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: ["posts"],
-    queryFn: () =>
-      apiRequest
-        .get(`questions?category=${catQuery}&sortBy=${sortQuery}`)
-        .then((res) => {
-          return res.data;
-        }),
+    queryFn: fetchPosts,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      return lastPage.currentPage < lastPage.totalPages
+        ? lastPage.currentPage + 1
+        : undefined;
+    },
   });
   useEffect(() => {
     refetch();
   }, [catQuery, sortQuery, refetch]);
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
+
+  const posts = isSuccess ? data.pages.flatMap((page) => page.questions) : [];
+
   return (
     <div className="w-full flex flex-col gap-4">
       {isLoading || isRefetching ? (
         Array(3)
           .fill(null)
           .map((_, index) => <PostCardSkeleton key={index} />)
-      ) : error ? (
+      ) : isError ? (
         <Retry refetch={refetch} />
-      ) : posts?.length === 0 ? (
+      ) : posts.length === 0 ? (
         <NoData message="No Questions at the moment" />
       ) : (
-        posts?.map((post) => <PostCard key={post._id} post={post} />)
+        <div className="w-full flex flex-col gap-4">
+          {posts.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+          <div ref={ref} className=" mx-auto my-6">
+            {isFetchingNextPage && (
+              <LucideLoaderCircle className=" w-5 h-5 text-blue-600 animate-spin" />
+            )}
+          </div>
+        </div>
       )}
     </div>
   );
