9dde0a90edd72a56570a9aa0ff804061448416d4
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2024-07-12 11:52:53 +0530

    feat: added Accordion for rendering Answer summary

--- frontend/src/components/answers/AnswerSummary.tsx
+++ frontend/src/components/answers/AnswerSummary.tsx
@@ -4,6 +4,12 @@ import { useEffect, useState } from "react";
 import apiRequest from "@/lib/apiRequest";
 import { useParams } from "react-router-dom";
 import { Skeleton } from "../ui/skeleton";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 interface AnswerSummaryProps {
   answers: Answer[];
@@ -47,27 +53,37 @@ const AnswerSummary = ({ answers }: AnswerSummaryProps) => {
   }, [answers, id]);
   return (
     ansCount >= 2 && (
-      <article className="mt-2 md:mt-4 w-full flex flex-col gap-2">
-        <div
-          className={`flex gap-2 items-center ${
-            answerSummary ? "animate-none" : "animate-pulse"
-          }`}
-        >
-          <h2 className=" text-xs md:text-sm bg-gradient-to-r from-blue-700 via-pink-600 to-red-800 bg-clip-text text-transparent">
-            {answerSummary
-              ? "AI-generated Answer Summary"
-              : "Summarizing Answers"}
-          </h2>
-          <BsStars className=" w-4 md:w-5  h-4 md:h-5 text-blue-600 " />
-        </div>
-        {answerSummary ? (
-          <p className=" leading-5  text-gray-800 pb-2">{answerSummary}</p>
-        ) : (
-          <div className=" space-y-1">
-            <Skeleton className=" w-full h-5 bg-gray-600/10" />
-            <Skeleton className=" w-full h-5 bg-gray-600/10" />
-          </div>
-        )}
+      <article className=" w-full flex flex-col gap-2">
+        <Accordion type="single" defaultValue="answer-summary" collapsible className=" bg-white px-2 border-1 border-gray-300">
+          <AccordionItem value="answer-summary">
+            <AccordionTrigger className="">
+              <div
+                className={`flex gap-2 items-center ${
+                  answerSummary ? "animate-none" : "animate-pulse"
+                }`}
+              >
+                <h2 className=" text-xs md:text-sm bg-gradient-to-r from-blue-700 via-pink-600 to-red-800 bg-clip-text text-transparent">
+                  {answerSummary
+                    ? "AI-generated Answer Summary"
+                    : "Summarizing Answers"}
+                </h2>
+                <BsStars className=" w-4 md:w-5  h-4 md:h-5 text-blue-600 " />
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>
+              {answerSummary ? (
+                <p className=" leading-5  text-gray-800 pb-2">
+                  {answerSummary}
+                </p>
+              ) : (
+                <div className=" space-y-1">
+                  <Skeleton className=" w-full h-5 bg-gray-600/10" />
+                  <Skeleton className=" w-full h-5 bg-gray-600/10" />
+                </div>
+              )}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </article>
     )
   );
